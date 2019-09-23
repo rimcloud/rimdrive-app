e1f5ab4cfa41113ec37a301a1b3f478dea33e1f3
syhan <newsyhan@gmail.com>
2019-09-24 01:04:09 +0900

    electron 설정과 실행 오류 해결

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React2
         </a>
       </header>
     </div>
