39312b26e08550d26c4287f6d70abed11887dd0d
sangyong han <syhan@cloudrim.co.kr>
2019-09-25 15:41:08 +0900

    설정 파일 정보를 스토어에 설정하는 액션 추가

--- src/modules/GlobalModule.js
+++ src/modules/GlobalModule.js
@@ -2,6 +2,8 @@
 import { handleActions } from 'redux-actions';
 import { Map } from 'immutable';
 
+const INIT_SYNCDATA_SUCCESS = 'global/INIT_SYNCDATA_SUCCESS';
+
 const SHOW_ELEMENT_MESSAGE = 'global/SHOW_ELEMENT_MESSAGE';
 const CLOSE_ELEMENT_MESSAGE = 'global/CLOSE_ELEMENT_MESSAGE';
 
@@ -10,7 +12,8 @@ const CHG_STORE_DATA = 'global/CHG_STORE_DATA';
 // ...
 const initialState = Map({
     popoverElement: null,
-    popoverText: ''
+    popoverText: '',
+    syncData: ''
 });
 
 export const showElementMsg = (elementObj, text) => dispatch => {
@@ -20,18 +23,25 @@ export const showElementMsg = (elementObj, text) => dispatch => {
         text: text
     });
 };
+
 export const closeElementMsg = (param) => dispatch => {
     return dispatch({
         type: CLOSE_ELEMENT_MESSAGE
     });
 };
 
-
 export const changeStoreData = (param) => dispatch => {
     return dispatch({
         type: CHG_STORE_DATA,
         name: param.name,
         value: param.value
+    });
+};
+
+export const initSyncData = (param) => dispatch => {
+    return dispatch({
+        type: INIT_SYNCDATA_SUCCESS,
+        syncData: param.syncData
     });
 };
 
@@ -50,6 +60,11 @@ export default handleActions({
         return state.merge({
             popoverElement: null,
             popoverText: ''
+        });
+    },
+    [INIT_SYNCDATA_SUCCESS]: (state, action) => {
+        return state.merge({
+            syncData: action.syncData
         });
     },
 
