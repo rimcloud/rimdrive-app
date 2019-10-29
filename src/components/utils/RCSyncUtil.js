d1611058df5d4920c3ba490213e52629689a8592
sangyong han <syhan@cloudrim.co.kr>
2019-10-29 16:58:35 +0900

    동기화에 각 프로세스에 폴더와 파일 구분을 공통 함수로 분리

--- src/components/utils/RCSyncUtil.js
+++ src/components/utils/RCSyncUtil.js
@@ -110,8 +110,8 @@ export function getCloudFiles(syncItem) {
 
 const fileUpload = (localFile, cloudTarget) => {
   const serverUrl = 'http://demo-ni.cloudrim.co.kr:48080/vdrive/file/api/files.ros';
-  const filePath = `${localFile.targetPath}${localFile.relPath}`;
-
+  const filePath = path.normalize(localFile.targetPath + localFile.relPath);
+console.log('fileupload == filePath :: ', filePath);
   const bbFile = new Blob([fs.readFileSync(filePath)]);
   const form_data = new FormData();
   form_data.append('rimUploadFile', bbFile, path.basename(filePath));
@@ -150,10 +150,22 @@ const createCloudFolder = (cloudTarget, localFile) => {
   }
 }
 
-const deleteCloudFolder = (cloudTarget, localFile) => {
+const deleteCloudFolder = (cloudTarget, cloudFile) => {
   const ipcResult = ipcRenderer.sendSync('get-data-from-server', {
     url: 'demo-ni.cloudrim.co.kr:48080/vdrive/file/api/files.ros',
-    params: `method=DELDIR&userid=test01&path=/개인저장소/모든파일/${cloudTarget + localFile.relPath}`
+    params: `method=DELDIR&userid=test01&path=/개인저장소/모든파일/${cloudTarget + cloudFile.relPath}`
+  });
+  if (ipcResult && ipcResult.status && ipcResult.status.result === 'SUCCESS') {
+    // uploadResult = 'SUCCESS';
+  } else {
+    // uploadResult = 'FAIL';
+  }
+}
+
+const deleteCloudFile = (cloudTarget, cloudFile) => {
+  const ipcResult = ipcRenderer.sendSync('get-data-from-server', {
+    url: 'demo-ni.cloudrim.co.kr:48080/vdrive/file/api/files.ros',
+    params: `method=DELFILE&userid=test01&path=/개인저장소/모든파일/${cloudTarget + cloudFile.relPath}`
   });
   if (ipcResult && ipcResult.status && ipcResult.status.result === 'SUCCESS') {
     // uploadResult = 'SUCCESS';
@@ -193,6 +205,47 @@ const createStateItem = (file, localTarget, cloudTarget) => {
   }
 }
 
+const syncLocalToCloud = (localFile, cloudTarget) => {
+  if (localFile.type === 'D') {
+    // CREATE FOLDER TO CLOUD
+    createCloudFolder(cloudTarget, localFile);
+  } else {
+    // UPLOAD FILE TO CLOUD
+    fileUpload(localFile, cloudTarget);
+  }
+}
+
+const syncLocalDelete = (localFile) => {
+  if (localFile.type === 'D') {
+    // DELETE LOCAL FOLDER
+    deleteFolderRecursive(path.normalize(localFile.targetPath + localFile.relPath));
+  } else {
+    // DELETE LOCAL FILE
+    fs.unlinkSync(path.normalize(localFile.targetPath + localFile.relPath));
+  }              
+}
+
+const syncCloudToLocal = (cloudFile, localTarget) => {
+  if (cloudFile.type === 'D') {
+    // CREATE FOLDER TO LOCAL
+    fs.mkdirSync(`${localTarget}${cloudFile.relPath}/${cloudFile.name}`);
+  } else {
+    // DOWNLOAD FILE TO CLOUD
+    fileDownload(cloudFile, localTarget);
+  }
+}
+
+const syncCloudDelete = (cloudFile, cloudTarget) => {
+  if (cloudFile.type === 'D') {
+    // DELETE CLOUD FOLDER
+    deleteCloudFolder(cloudTarget, cloudFile);
+  } else {
+    // DELETE CLOUD FILE
+    deleteCloudFile(cloudTarget, cloudFile);
+  }              
+}
+
+
 export function startCompareData(localDB, cloudDB, localTarget, cloudTarget) {
 
   return new Promise(function (resolve, reject) {
@@ -212,16 +265,9 @@ export function startCompareData(localDB, cloudDB, localTarget, cloudTarget) {
       localFiles.forEach((localFile, i) => {
         const cloudFile = cloudDB.get('files').find({ pathHash: localFile.pathHash }).value();
         if (cloudFile === null || cloudFile === undefined) {
-          // 클라우드데이터에는 없음. 클라우드에 폴더 생성 또는 파이 ㄹ업로드
-          if (localFile.type === 'D') {
-            // CREATE FOLDER TO CLOUD
-            createCloudFolder(cloudTarget, localFile);
-            innerItems.push(createStateItem(localFile, localTarget, cloudTarget));
-          } else {
-            // UPLOAD TO CLOUD
-            fileUpload(localFile, cloudTarget);
-            innerItems.push(createStateItem(localFile, localTarget, cloudTarget));
-          }
+          // LOCAL => CLOUD    // 클라우드데이터에는 없음. 클라우드에 폴더 생성 또는 파일업로드
+          syncLocalToCloud(localFile, cloudTarget);
+          innerItems.push(createStateItem(localFile, localTarget, cloudTarget));
         }
       });
 
@@ -233,8 +279,8 @@ export function startCompareData(localDB, cloudDB, localTarget, cloudTarget) {
         const localFile = localDB.get('files').find({ pathHash: cloudFile.pathHash }).value();
         if (localFile === null || localFile === undefined) {
           if (cloudFile.type === 'D') {
-            // 로컬에 폴더 생성
-            fs.mkdirSync(`${localTarget}${cloudFile.relPath}/${cloudFile.name}`);
+            // CLOUD => LOCAL
+            syncCloudToLocal(cloudFile, localTarget);
             innerItems.push(createStateItem(cloudFile, localTarget, cloudTarget));
           }
         }
@@ -244,8 +290,8 @@ export function startCompareData(localDB, cloudDB, localTarget, cloudTarget) {
         const localFile = localDB.get('files').find({ pathHash: cloudFile.pathHash }).value();
         if (localFile === null || localFile === undefined) {
           if (cloudFile.type === 'F') {
-            // 클라우드에서 파일 다운로드
-            fileDownload(cloudFile, localTarget);
+            // CLOUD => LOCAL
+            syncCloudToLocal(cloudFile, localTarget);
             innerItems.push(createStateItem(cloudFile, localTarget, cloudTarget));
           }
         }
@@ -255,7 +301,7 @@ export function startCompareData(localDB, cloudDB, localTarget, cloudTarget) {
       stateDB.assign({ files: innerItems }).write();
 
     } else {
-      
+
       let innerItems = [];
       // compare data by three times.
 
@@ -264,13 +310,13 @@ export function startCompareData(localDB, cloudDB, localTarget, cloudTarget) {
       const localFiles = localDB.get('files').value();
       localFiles.forEach((localFile, i) => {
 
-          // ##### localFile.mtime === stateFile.local_mtime
-          // state file 에 local_mtime 은 로컬 시간으로 재조정 해야 한다~~~~~~!!!!!!!!!!!!!!!!
+        // ##### localFile.mtime === stateFile.local_mtime
+        // state file 에 local_mtime 은 로컬 시간으로 재조정 해야 한다~~~~~~!!!!!!!!!!!!!!!!
 
-        console.log('======================================================');
-        console.log(' :: localFile :: ', localFile);
-        console.log(' :: localFile path :: ', path.normalize(localFile.targetPath + localFile.relPath));
-        console.log('======================================================');
+        // console.log('======================================================');
+        // console.log(' :: localFile :: ', localFile);
+        // console.log(' :: localFile path :: ', path.normalize(localFile.targetPath + localFile.relPath));
+        // console.log('======================================================');
         
         const cloudFile = cloudDB.get('files').find({ pathHash: localFile.pathHash }).value();
         const stateFile = stateDB.get('files').find({ pathHash: localFile.pathHash }).value();
@@ -278,23 +324,20 @@ export function startCompareData(localDB, cloudDB, localTarget, cloudTarget) {
         if (cloudFile === null || cloudFile === undefined) {
           // 클라우드데이터에 없다
           if (stateFile === null || stateFile === undefined) {
-            // 작업데이터에 없다 - 클라우드에 폴더생성, 파일업로드
-            if (localFile.type === 'D') {
-              // CREATE FOLDER TO CLOUD
-              createCloudFolder(cloudTarget, localFile);
-            } else {
-              // UPLOAD TO CLOUD
-              fileUpload(localFile, cloudTarget);
-            }
+            // 작업데이터에 없다 - 로컬에 신규로 생긴 데이터
+            // LOCAL => CLOUD
+            syncLocalToCloud(localFile, cloudTarget);
+            innerItems.push(createStateItem(localFile, localTarget, cloudTarget));
           } else {
             // 작업데이터에는 있다 - 삭제 또는 클라우드에 생성
             if (localFile.size === stateFile.size && localFile.mtime === stateFile.local_mtime) {
               // 로컬데이터와 작업데이터가 같은면 로컬 삭제
-              // DELETE LOCAL FILE
-              deleteFolderRecursive(path.normalize(localFile.targetPath + localFile.relPath));
+              // LOCAL DELETE
+              syncLocalDelete(localFile);
             } else {
-              // UPLOAD TO CLOUD
-              fileUpload(localFile, cloudTarget);
+              // LOCAL => CLOUD
+              syncLocalToCloud(localFile, cloudTarget);
+              innerItems.push(createStateItem(localFile, localTarget, cloudTarget));
             }
           }
         } else {
@@ -302,26 +345,30 @@ export function startCompareData(localDB, cloudDB, localTarget, cloudTarget) {
           if (stateFile === null || stateFile === undefined) {
             // 작업데이터에는 없다 (즉, 로컬과 클라우드의 각각 새롭게 생성되었음)
             if (localFile.mtime > cloudFile.mtime) {
-              // 로컬데이터가 최신이므로 업로드
-              // UPLOAD TO CLOUD
-              fileUpload(localFile, cloudTarget);
+              // 로컬데이터가 최신이므로 클라우드에 폴더 생성 또는 파일 업로드
+              // LOCAL => CLOUD
+              syncLocalToCloud(localFile, cloudTarget);
+              innerItems.push(createStateItem(localFile, localTarget, cloudTarget));
             } else {
-              // 클라우드데이터가 최신이므로 다운로드
-              // DOWNLOAD TO LOCAL
-              fileDownload(cloudFile, localTarget);
+              // 클라우드데이터가 최신이므로 로컬에 폴더 생성 또는 파일 다운로드
+              // CLOUD => LOCAL
+              syncCloudToLocal(cloudFile, localTarget);
+              innerItems.push(createStateItem(cloudFile, localTarget, cloudTarget));
             }
           } else {
             // 작업데이터에도 있다 (즉, 로컬과 클라우드의 각각 비교후 처리여부 확인)
             if (localFile.size === stateFile.size && localFile.mtime === stateFile.local_mtime) {
               // 동기화 되어 있는 상태, 작업 필요 없음
             } else if (localFile.mtime > cloudFile.mtime) {
-              // 로컬데이터가 최신이므로 업로드
-              // UPLOAD TO CLOUD
-              fileUpload(localFile, cloudTarget);
+               // 로컬데이터가 최신이므로 클라우드에 폴더 생성 또는 파일 업로드
+              // LOCAL => CLOUD
+              syncLocalToCloud(localFile, cloudTarget);
+              innerItems.push(createStateItem(localFile, localTarget, cloudTarget));
             } else {
-              // 클라우드데이터가 최신이므로 다운로드
-              // DOWNLOAD TO LOCAL
-              fileDownload(cloudFile, localTarget);
+              // 클라우드데이터가 최신이므로 로컬에 폴더 생성 또는 파일 다운로드
+              // CLOUD => LOCAL
+              syncCloudToLocal(cloudFile, localTarget);
+              innerItems.push(createStateItem(cloudFile, localTarget, cloudTarget));
             }
           }
         }
@@ -339,21 +386,19 @@ export function startCompareData(localDB, cloudDB, localTarget, cloudTarget) {
           // 로컬데이터에 없다
           if (stateFile === null || stateFile === undefined) {
             // 작업데이터에도 없다 (클라우드에 새로생긴 정보, 폴더생성, 파일 다운로드 필요)
-            if (cloudFile.type === 'D') {
-              // 클라우드데이터가 최신임으로 로컬에 폴더 생성 // CREATE FOLDER
-              fs.mkdirSync(`${localTarget}${cloudFile.relPath}/${cloudFile.name}`);
-            } else {
-              // 클라우드데이터가 최신임으로 다운로드 // DOWNLOAD TO LOCAL
-              fileDownload(cloudFile, localTarget);
-            }
+              // CLOUD => LOCAL
+              syncCloudToLocal(cloudFile, localTarget);
+              innerItems.push(createStateItem(cloudFile, localTarget, cloudTarget));
           } else {
             // 작업데이터에는 있다. (클라우드에만 있음, 비교후 클라우드 삭제 또는 다운로드)
             if (cloudFile.size === stateFile.size && cloudFile.mtime === stateFile.cloud_mtime) {
               // 클라우드데이터가 오래된 정보 클라우드 삭제 // DELETE CLOUD FILE
-              deleteCloudFolder(cloudTarget, localFile);
+              syncCloudDelete(cloudFile, cloudTarget);
             } else {
               // 클라우드데이터가 최신임으로 다운로드 // DOWNLOAD TO LOCAL
-              fileDownload(cloudFile, localTarget);
+              // CLOUD => LOCAL
+              syncCloudToLocal(cloudFile, localTarget);
+              innerItems.push(createStateItem(cloudFile, localTarget, cloudTarget));
             }
           }
         }
@@ -377,8 +422,7 @@ export function startCompareData(localDB, cloudDB, localTarget, cloudTarget) {
       });
 
       // RE-Create state database 
-      stateDB.assign({ files: innerItems }).write();
-
+      stateDB.set({ files: []}).assign({ files: innerItems }).write();
     }
 
   //   let c = 0;
